98ad984042748ac49aee26c56eb2ff4f50723d9e
rupamghosh2006 <rupamgh2006@gmail.com>
2026-04-23 21:17:29 +0530

    updated frontend

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gateway.pinata.cloud',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.stellar.org',
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000'],
-    },
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
